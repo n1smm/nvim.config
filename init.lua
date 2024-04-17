14f546b7f1f4c92966f2ce6a6666f3808a622516
Tjaz Juvan <tjuvan@c1r2p1.42firenze.it>
2024-04-17 19:41:33 +0200

    first commit of .config for nvim

---
+++ init.lua
@@ -0,0 +1,2 @@
+require("nism")
+print("hello")
---
+++ lua/nism/init.lua
@@ -0,0 +1,6 @@
+require("nism.remap")
+print("hello from nism")
+--vim.cmd("colorscheme citruszest")
+--vim.cmd("colorscheme miasma")
+vim.opt.termguicolors = true
+vim.cmd.colorscheme 'melange'
