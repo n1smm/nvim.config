9d63ffca1f0f520590fe0d6a93a0bd684638c610
Tjaz Juvan <tjuvan@c2r4p12.42firenze.it>
2024-05-15 20:31:13 +0200

    some changes to copilot and telescope file browser, need tp install tailwind tools

--- lua/nism/remap.lua
+++ lua/nism/remap.lua
@@ -33,7 +33,7 @@ vim.keymap.set('n', '<leader>wq', '<cmd>lua WriteQuit()<CR>', { noremap = true, 
 vim.keymap.set("n", "<leader>ww", vim.cmd.wall)
 
 -- switch windows ; w = Window hjkl= direction
-vim.keymap.set("n", "<leader>wl", '<C-w>l') 
+vim.keymap.set("n", "<leader>wl", '<C-w>l')
 vim.keymap.set("n", "<leader>wh", '<C-w>h')
 vim.keymap.set("n", "<leader>wj", '<C-w>j')
 vim.keymap.set("n", "<leader>wk", '<C-w>k')
--- plugin/after/plugin/telescope.lua
+++ plugin/after/plugin/telescope.lua
@@ -1,6 +1,35 @@
 local builtin = require('telescope.builtin')
+local fb_actions = require "telescope".extensions.file_browser.actions
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fw',function() 
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+
+-- telescope file browser
+vim.keymap.set("n", "<space>fb", ":Telescope file_browser<CR>")
+require("telescope").setup {
+  extensions = {
+    file_browser = {
+      theme = "ivy",
+      --hijack_netrw = true,
+      mappings = {
+        ["i"] = {
+          -- your custom insert mode mappings
+        },
+        ["n"] = {
+			["%"] = fb_actions.create,
+			["R"] = fb_actions.rename,
+			["D"] = fb_actions.remove,
+			["Y"] = fb_actions.copy,
+			-- ["P"] = fb_actions.paste,
+			["."] = fb_actions.toggle_hidden,
+			["-"] = fb_actions.goto_parent_dir,
+			["~"] = fb_actions.goto_home_dir,
+			--toggle browser <C-f>
+        },
+      },
+    },
+  },
+}
+require("telescope").load_extension "file_browser"
